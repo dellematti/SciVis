79c397dd635c782b8afeb9d44c8e13b38a73ddc4
LucaCorra02 <119894551+LucaCorra02@users.noreply.github.com>
2023-06-12 17:12:12 +0200

    Add files via upload

--- StataleVsMessina/ConfrontoVoti2022.py
+++ StataleVsMessina/ConfrontoVoti2022.py
@@ -23,12 +23,28 @@ def votiStatale():
 
     return passati.values()
 
-def grafo():#Anno 2022
-    insubria = [0,71, 29, 7, 4, 3] #ottenute da scripit EstraivotiInsubria
+def grafo():#Anno 2021-2022
+    Messina = [0,71, 29, 7, 4, 3] #ottenute da scripit EstraiMessian
+    totMessina = 0 
+    totStatale = 0
+    for i in Messina : 
+        totMessina+=i
+
     statale = votiStatale()
     statale =  [i for i in statale]
+    for i in statale : 
+        totStatale+=i
+    
+    for i in range (1,len(Messina)):
+        Messina[i]=round((Messina[i]/totMessina)*100)
+        
+    for i in range (0,len(statale)):
+        statale[i]=round((statale[i]/totStatale)*100)
+    
     statale.insert(0,0)
+    statale[1]+=1
     print(statale)
+    print(Messina)
     
     x = ["0","Gennaio","Febbraio","Giugno","Luglio","Settembre"]
     
@@ -37,11 +53,30 @@ def grafo():#Anno 2022
     ax = fig.add_subplot(1, 1, 1)
     ax.grid(axis='y',color = '#d9d9d9', linestyle = '-', linewidth = 0.5)
 
-    plt.fill_between(x, insubria, color="lightpink",alpha=0.5,label='Insubria')
+    plt.fill_between(x, Messina, color="lightpink",alpha=0.5,label='Messina')
     plt.fill_between(x, statale, color="skyblue",alpha=0.5, label='Statle')
     plt.ylim(-10,100)
-    plt.plot(x, insubria,linestyle='-', marker='.', color="lightpink")
+    plt.plot(x, Messina,linestyle='-', marker='.', color="lightpink")
     plt.plot(x, statale,linestyle='-', marker='.', color="skyblue")
+    
+    for tmpx,tmpy in zip(x,Messina):
+        print(tmpy)
+        label = str(tmpy)+"%"
+        plt.annotate(label,
+                 (tmpx,tmpy),
+                 textcoords="offset points",
+                 xytext=(0,10),
+                 ha='center',
+                 arrowprops=dict(arrowstyle="->", color='lightpink'))
+        
+    for tmpx,tmpy in zip(x,statale):
+        label = str(tmpy)+"%"
+        plt.annotate(label,
+                 (tmpx,tmpy),
+                 textcoords="offset points",
+                 xytext=(0,20),
+                 ha='center',
+                 arrowprops=dict(arrowstyle="->", color='skyblue'))
 
     #spazio = 0.400
     #fig.subplots_adjust(bottom=spazio)
@@ -49,10 +84,10 @@ def grafo():#Anno 2022
     plt.legend()
     ax = plt.gca()
     ax.set_facecolor("#f6faf9")
-    plt.ylabel('TotPassati')
+    plt.ylabel('Percentuale')
 
     plt.show()
-    plt.savefig("ce.png") 
+    plt.savefig("anno2020.svg") 
    
 
 grafo()
--- StataleVsMessina/SmallMultipleAtenei.py
+++ StataleVsMessina/SmallMultipleAtenei.py
@@ -1,48 +1,65 @@
-import numpy as np
-import seaborn as sns
+
 import pandas as pd
 import matplotlib.pyplot as plt
 """" 
-anno 2019 = [83,58,10,5,9]
+anno 2019 = [63,38,10,5,9]
 anno 2020 = [52,33,18,5,7]
-anno 2021 = [35,62,9,2,5]
+anno 2021 = [30,62,9,2,5]
 anno 2022 = [71,29,7,4,3]
 """
-
 """"
 Statle : 
+    2019 = {'Gennaio': 30, 'Febbraio': 38, 'Giugno': 13, 'Luglio': 13, 'Settembre': 15}
     anno 2020 = {'Gennaio': 33, 'Febbraio': 27, 'Giugno': 12, 'Luglio': 7, 'Settembre': 11}
     anno 2021 = {'Gennaio': 38, 'Febbraio': 25, 'Giugno': 38, 'Luglio': 11, 'Settembre': 10}
     anno= 2022 {'Gennaio': 9, 'Febbraio': 44, 'Giugno': 13, 'Luglio': 7, 'Settembre': 7}}
 """
-# Create a datase
-y = [71,29,7,4,3]
+y = [30,38,13,13,15]
+tot=0
+for i in y :
+    tot+=i
+    
+for i in range (0,len(y)):
+    y[i]= round((y[i]/tot)*100)
+    
+print(y)
 x= ["Gennaio","Febbraio","Giugno","Luglio","Settembre"]
 fig = plt.figure()
 fig.patch.set_facecolor("#f6faf9")
 
 
-plt.fill_between(x, y, color="lightpink",alpha=0.5,label='Insubria')
-#plt.fill_between(x, y, color="skyblue",alpha=0.5, label='Statle')
+#plt.fill_between(x, y, color="lightpink",alpha=0.5,label='Messina')
+plt.fill_between(x, y, color="skyblue",alpha=0.5, label='Statle')
 plt.ylim(-4,90)
-plt.plot(x, y,linestyle='-', marker='.', color="lightpink")
-#plt.plot(x, y,linestyle='-', marker='.', color="skyblue")
+#plt.plot(x, y,linestyle='-', marker='.', color="lightpink")
+plt.plot(x, y,linestyle='-', marker='.', color="skyblue")
 
 #spazio = 0.600
 #fig.subplots_adjust(bottom=spazio)
 ax = plt.gca()
 ax.set_facecolor("#f6faf9")
-#plt.ylabel('Passati')
+plt.ylabel('Passati %')
 plt.xlabel('Mesi')
 
 ax = plt.gca()
 #ax.axes.xaxis.set_ticklabels([]) #rimuovono le lables sull'asse delle x o y
-ax.axes.yaxis.set_ticklabels([])
+#ax.axes.yaxis.set_ticklabels([])
 
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
-#plt.title("Statale 2022")
-plt.title("Messina 2022")
+#plt.title("Messina 2019")
+plt.title("Statale 2019")
+
+for tmpx,tmpy in zip(x,y):
+    print(tmpy)
+    label = str(tmpy)+"%"
+    plt.annotate(label,
+                (tmpx,tmpy),
+                textcoords="offset points",
+                xytext=(0,10),
+                ha='center',
+                #skyblue
+                arrowprops=dict(arrowstyle="->", color='skyblue'))
 
 plt.show()
 plt.savefig("smallMultiple.svg") 
