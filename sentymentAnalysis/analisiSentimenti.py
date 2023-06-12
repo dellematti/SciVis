a5a4aa5223e3f24fda901265c078f514b88aed1c
LucaCorra02 <119894551+LucaCorra02@users.noreply.github.com>
2023-06-12 17:13:07 +0200

    Add files via upload

--- sentymentAnalysis/analisiSentimenti.py
+++ sentymentAnalysis/analisiSentimenti.py
@@ -1,11 +1,9 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-#from sklearn.linear_model import
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
@@ -20,7 +18,7 @@ def modello():
     x = vect.fit_transform(x) #uso modello bag of words.
 
     x_train, x_test, y_train, y_test = train_test_split(x.toarray(),y,test_size=0.30, random_state=0) #divido il data set in 2 una parte per l'addestramento una per il test
-    model = DecisionTreeClassifier() #creo il modello vuoto non addestrato
+    model = DecisionTreeClassifier(max_depth=15) #creo il modello vuoto non addestrato
     model.fit(x_train, y_train)
 
     predizioneTrain = model.predict(x_train)
@@ -29,5 +27,10 @@ def modello():
     risTest = accuracy_score(y_test,predizioneTest)
 
     print(f'Train acc. {risTrain}, Test acc.{risTest}')
+    
+    plt.figure()
+    tree.plot_tree(model,filled=True)  
+    plt.savefig('tree.svg',format='svg',bbox_inches = "tight")
+    
 
 modello()
